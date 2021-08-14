b2931e6cb51ea2555064e47410cc8df71b9a3a4c
Evgenijstr <evgenijstr@gmail.com>
2021-08-14 17:11:25 +0300

    'main.java created'

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long currentAmount = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        long total = currentAmount + transferAmount;
+        System.out.println(total);
+    }
+}
